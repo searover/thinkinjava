5ae0cd55b67787a18c5f9dce85f27903bb87114d
Searover <ibaiguang@gmail.com>
2016-01-11 22:15:51 +0800

    Add exist codes.

---
+++ object/HelloDate.java
@@ -0,0 +1,22 @@
+//: object/HelloDate.java
+import java.util.*;
+
+/**
+ * The first Thinking in java example program.
+ * Displays a string and today's date.
+ * @author Searover
+ * @author www.ibaiguang.com
+ * @version 1.0
+ */
+public class HelloDate{
+		
+		/**
+		 * Entrv point to class & application.
+		 * @param args array of string arguments
+		 * @throws exceptions No exception thrown
+		 */
+		public static void main(String[] args){
+				System.out.println("Hello, it's: ");	
+				System.out.println(new Date());
+		}
+}
