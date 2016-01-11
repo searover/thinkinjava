26860639862a8684b66bae17736d414e97735f77
Searover <ibaiguang@gmail.com>
2016-01-11 23:39:46 +0800

    Add control's code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # thinkinjava
 Exercises from the book "**Think in Java**"
+
+1, Java SE5 引入了一种新的更加简洁的for语法用于数组和容器，即foreach语法，表示不必创建int变量去对由访问项构成的序列进行计数，foreach将自动产生每一项。
---
+++ operations/Exponents.java
@@ -0,0 +1,10 @@
+public class Exponents{
+    public static void main(String[] args){
+        // Uppercase and lowercase 'e' are the same.
+        float expFloat = 1.39e-43f;
+        System.out.println(expFloat);
+        double expDouble = 47e47d;
+        double expDouble2 = 47e47;
+        System.out.println(expDouble);
+    }
+}
