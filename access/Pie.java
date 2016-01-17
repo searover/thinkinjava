85bc2568fa966438ab71c764dee87f5bd124fd29
searover <ibaiguang@gmail.com>
2016-01-18 02:22:38 +0800

    Add reusing code

---
+++ access/Pie.java
@@ -0,0 +1,7 @@
+// The other class
+
+class Pie {
+    void f(){
+        System.out.println("Pie.f()");
+    }
+}
---
+++ access/dessert/Cookie.java
@@ -0,0 +1,11 @@
+// Create a library
+package access.dessert;
+
+public class Cookie {
+    public Cookie(){
+        System.out.println("Cookie constructor");
+    }
+    void bite(){
+        System.out.println("bite");
+    }
+}
