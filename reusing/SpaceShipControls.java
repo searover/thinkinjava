aff4d46b34a29da8d914ac115120a15c3ae2971f
searover <ibaiguang@gmail.com>
2016-01-20 20:14:58 +0800

    Add extends class code

---
+++ reusing/SpaceShipControls.java
@@ -0,0 +1,9 @@
+public class SpaceShipControls{
+    void up(int velocity) {}
+    void down(int velocity) {}
+    void left(int velocity) {}
+    void right(int velocity) {}
+    void forward(int velocity) {}
+    void back(int velocity) {}
+    void turboBoost() {}
+}
