f30429a545bf76e19067b1647d06dd3e277ada7d
searover <ibaiguang@gmail.com>
2016-02-29 16:52:04 +0800

    Add spring boot code

---
+++ polymorphism/music/Instrument.java
@@ -0,0 +1,9 @@
+// :polymorphism/music/Instrument.java
+package polymorphism.music;
+import static net.mindview.util.Print.*;
+
+class Instrument {
+    public void play(Note n){
+        print("Instrument.play()");
+    }
+}
