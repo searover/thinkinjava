84a109682c433ab18c9ecb800dd9e82a8246833c
searover <ibaiguang@gmail.com>
2016-03-06 18:55:11 +0800

    Add Polymorphic Constructor, Field Access in Polymorphic, and Covariant Return Code

---
+++ Polymorphism/src/polymorphism/CovariantReturn.java
@@ -0,0 +1,42 @@
+package polymorphism;
+
+class Grain{
+    @Override
+    public String toString() {
+        return "Grain";
+    }
+}
+
+class Wheat extends Grain{
+    @Override
+    public String toString() {
+        return "Wheat";
+    }
+}
+
+class Mill{
+    Grain process(){
+        return new Grain();
+    }
+}
+
+class WheatMill extends Mill{
+    @Override
+    Wheat process() {
+        return new Wheat();
+    }
+}
+
+/**
+ * Created by searover on 3/6/16.
+ */
+public class CovariantReturn {
+    public static void main(String[] args) {
+        Mill m = new Mill();
+        Grain g = m.process();
+        System.out.println(g);
+        m = new WheatMill();
+        g = m.process();
+        System.out.println(g);
+    }
+}
